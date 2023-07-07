23de8529bb9f3a647e8aa92866308cb04b2ab352
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-07 06:09:45 -0500

    memorize task

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,8 @@ import './index.css'
 // import Layout from '../05-useLayoutEffect/Layout'
 // import Memorize from '../06-memos/Memorize'
 // import MemoHook from '../06-memos/MemoHook'
-import CallbackHook from '../06-memos/CallbackHook'
+// import CallbackHook from '../06-memos/CallbackHook'
+import { Padre } from '../07-tarea-memo/Padre'
 // import SimpleFormWithCustomHook from '../02-useEffect/SimpleFormWithCustomHook'
 // import MultipleCustomHooks from '../03-useFetch/MultipleCustomHooks'
 // import FocusScreen from '../04-useRef/FocusScreen'
@@ -15,6 +16,6 @@ import CallbackHook from '../06-memos/CallbackHook'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
-  <CallbackHook />
+  <Padre />
   // </React.StrictMode>,
 )

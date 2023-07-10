5a16a7f8a678c928f38f67f66f9e1749c60c10e5
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-10 07:20:52 -0500

    useReducer introduction

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 // import Memorize from '../06-memos/Memorize'
 // import MemoHook from '../06-memos/MemoHook'
 // import CallbackHook from '../06-memos/CallbackHook'
-import { Padre } from '../07-tarea-memo/Padre'
+// import { Padre } from '../07-tarea-memo/Padre'
 // import SimpleFormWithCustomHook from '../02-useEffect/SimpleFormWithCustomHook'
 // import MultipleCustomHooks from '../03-useFetch/MultipleCustomHooks'
 // import FocusScreen from '../04-useRef/FocusScreen'
@@ -13,9 +13,10 @@ import { Padre } from '../07-tarea-memo/Padre'
 // import CounterApp from '../01-useState/CounterApp'
 // import CounterCustomH from '../01-useState/CounterCustomHook'
 // import HooksApp from './HooksApp'
+import '../08-useReducer/intro-reducer'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <Padre />
-  // </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   // <React.StrictMode>
+//   <Padre />
+//   // </React.StrictMode>,
+// )

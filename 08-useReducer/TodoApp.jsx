e2caf36fc6b732e48ae0815df7d3f16842b8a3c8
Cristianps1988 <cristianps_1@hotmail.com>
2023-07-11 20:36:43 -0500

    Tarea done

--- 08-useReducer/TodoApp.jsx
+++ 08-useReducer/TodoApp.jsx
@@ -1,19 +1,26 @@
 import React, { useReducer } from 'react'
 import { todoReducer } from './todoReducer'
+import { TodoList } from './TodoList'
+import { TodoAdd } from './TodoAdd'
+
+const initialState = [{
+    id: Date.now(),
+    description: 'Doblar la ropa',
+    done: false
+},
+{
+    id: Date.now() + 1,
+    description: 'Lavar la losa',
+    done: false
+}]
 
 export const TodoApp = () => {
-    const initialState = [{
-        id: Date.now(),
-        description: 'Doblar la ropa',
-        done: false
-    },
-    {
-        id: Date.now() + 1,
-        description: 'Lavar la losa',
-        done: false
-    }
-    ]
     const [todos, dispatchTodo] = useReducer(todoReducer, initialState)
+
+    const handleNewTodo = (todo) => {
+        console.log({ todo })
+    }
+
     return (
         <>
             <h1>TodoApp: 10, <small>Pendientes: 2</small></h1>
@@ -21,34 +28,15 @@ export const TodoApp = () => {
             <div className='row'>
                 <div className='col-7'>
                     <ul className='list-group'>
-                        {
-                            todos.map(todo => {
-                                <li key={todo.id} className='list-group-item d-flex justify-content-between'>
-                                    <span className='align-self-center'>Tarea 1</span>
-                                    <button className='btn btn-danger'>Borrar</button>
-                                </li>
-                            })
-                        }
-
+                        <TodoList todos={todos} />
                     </ul>
                 </div>
 
                 <div className='col-5'>
+
                     <h4>Agregar TODO</h4>
                     <hr />
-                    <form>
-                        <input
-                            type="text"
-                            placeholder='¿Tirala plena mani?'
-                            className='form-control'
-                        />
-                        <button
-                            type='submit'
-                            className='btn btn-outline-primary mt-1'
-                        >
-                            Agregar
-                        </button>
-                    </form>
+                    <TodoAdd onNewTodo={handleNewTodo} ></TodoAdd>
                 </div>
 
             </div>
---
+++ 08-useReducer/TodoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { TodoItem } from './TodoItem'
+
+export const TodoList = ({ todos = [] }) => {
+    return (
+        <>
+            {
+                todos.map(todo => (
+                    <TodoItem key={todo.id} todo={todo}></TodoItem>
+                ))
+            }
+        </>
+    )
+}
